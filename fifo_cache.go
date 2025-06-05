f90d46d4a15dcedfb3313f3bf5d5ee4ac0fe8c60
DJJ <shuaiqijianhao@qq.com>
2025-06-05 19:30:10 +0800

    feat(cache):返回capacity

--- fifo_cache.go
+++ fifo_cache.go
@@ -75,7 +75,11 @@ func (c *FIFOCache[K, V]) ContainsWithFreq(key K) (ok bool, freq int32) {
 }
 
 func (c *FIFOCache[K, V]) Capacity() int {
-	return c.maxCapa
+	return c.maxCapacity
+}
+
+func (c *FIFOCache[K, V]) MaxCapacity() int {
+	return c.capacity
 }
 
 func (c *FIFOCache[K, V]) IncrFreq(key K) {
